b04ac567e08ce4baa92a4382ace36ee6cfa6b384
Ramesh Kumar <devarammca@gmail.com>
2023-03-04 12:38:16 +0530

    update to 14

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -7,11 +7,12 @@ import { LoginService } from 'src/app/services/login.service';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
 
   formGroup = new FormGroup({
-    username: new FormControl('',[Validators.required]),
-    password: new FormControl('',[Validators.required])
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required])
   })
 
   userIsAuthenticated: boolean = false;
@@ -21,20 +22,18 @@ export class LoginComponent implements OnInit {
     //this.initForm();
   }
 
-  initForm(){
+  initForm() {
     this.formGroup = new FormGroup({
-      username: new FormControl('',[Validators.required]),
-      password: new FormControl('',[Validators.required])
+      username: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required])
     })
   }
 
-  loginProcess()
-  {
-    if(this.formGroup.valid)
-    {
-      let username = this.formGroup.get("username")?.value;
-      let password = this.formGroup.get("password")?.value;;  
-      this._loginService.login(username, password);    
+  loginProcess() {
+    if (this.formGroup.valid) {
+      let username = (this.formGroup.value.username != null ? this.formGroup.value.username : "");
+      let password = (this.formGroup.value.password != null ? this.formGroup.value.password : "");;
+      this._loginService.login(username, password);
     }
   }
 
